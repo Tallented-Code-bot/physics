cc461cde4ac856a7c744dd42e06bac1081d0a59c
Calvinn Tallent <calvintallentspam@gmail.com>
2021-08-05 19:01:01 -0700

    add drawBall function to draw a ball

--- main.js
+++ main.js
@@ -1,2 +1,11 @@
 const canvas=document.getElementById("canvas");//get the canvas element
 const context=canvas.getContext("2d");//get the canvas drawing context so that we can draw
+
+function drawBall(x,y,radius){
+	context.beginPath();
+	context.arc(x,y,radius,0,2*Math.PI);
+	context.strokeStyle="black";
+	context.fillStyle="red";
+	context.stroke();
+	context.fill();
+}
