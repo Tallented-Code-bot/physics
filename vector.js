17e6eefb00a7dc8e1313233c5c07e7d6ecc56cc5
Tallent, Nathan R <nathan.tallent@pnnl.gov>
2021-08-06 12:48:05 -0700

    add some jsdoc comments

--- vector.js
+++ vector.js
@@ -1,9 +1,18 @@
+/**
+ * A basic vector class
+ * @param {number} x The x value of the vector
+ * @param {number} y The y value of the vector
+ */
 function Vector(x,y){
 	this.x=x;
 	this.y=y;
 }
 
-
+/**
+ * Adds two vectors together
+ * @param {Vector} vector The vector to add
+ * @returns The sum of the current vector and the new vector
+ */
 Vector.prototype.add=function(vector){
 	return new Vector(
 		this.x+vector.x,
@@ -11,6 +20,11 @@ Vector.prototype.add=function(vector){
 	);
 }
 
+/**
+ * Subtracts two vectors
+ * @param {Vector} vector The Vector to subtract
+ * @returns The difference of the current vector and the new vector
+ */
 Vector.prototype.sub=function(vector){
 	return new Vector(
 		this.x-vector.x,
@@ -18,12 +32,20 @@ Vector.prototype.sub=function(vector){
 	);
 }
 
-
+/**
+ * Gets the magnitude of the current vector
+ * @returns The magnitude of the vector
+ */
 Vector.prototype.mag=function(){
 	return Math.sqrt(this.x**2+this.y**2);
+	//use the pythagorean theorem to get the magnitude
 }
 
-
+/**
+ * Multiplies a vector and a scalar
+ * @param {number} number The number to multiply the vector by
+ * @returns The product of the current vector and the new vector
+ */
 Vector.prototype.mult=function(number){
 	return new Vector(
 		this.x*number,
@@ -31,6 +53,10 @@ Vector.prototype.mult=function(number){
 	);
 }
 
+/**
+ * Gets the unit vector from the current vector
+ * @returns The unit vector of the current vector
+ */
 Vector.prototype.unit=function(){
 	if(this.mag()===0)return new Vector(0,0);//this prevents division by 0
 	return new Vector(
@@ -39,7 +65,10 @@ Vector.prototype.unit=function(){
 	);
 }
 
-
+/**
+ * Gets the normal vector of the current vector
+ * @returns A vector perpendicular to the current vector
+ */
 Vector.prototype.normal=function(){
 	return new Vector(
 		-this.y,
@@ -47,7 +76,12 @@ Vector.prototype.normal=function(){
 	).unit();
 }
 
-
+/**
+ * Gets the dot product of two vectors.
+ * @param {Vector} vector1 The 1st vector to multiply
+ * @param {Vector} vector2 The 2nd vector to multiply
+ * @returns The dot product of the two vectors
+ */
 Vector.dot=function(vector1,vector2){
 	return vector1.x*vector2.x+vector1.y*vector2.y;
 }
