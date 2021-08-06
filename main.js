22e8c562c320609a6a2b7c672fca92f0b7229746
Calvinn Tallent <calvintallentspam@gmail.com>
2021-08-06 06:56:08 -0700

    Create a ball object, and move the moving and drawing functions to methods of it.  This will allow many balls to be created in the future.

--- main.js
+++ main.js
@@ -1,18 +1,33 @@
 const canvas=document.getElementById("canvas");//get the canvas element
 const context=canvas.getContext("2d");//get the canvas drawing context so that we can draw
 
-let x=100;
-let y=100;
 let LEFT,RIGHT,UP,DOWN;
 
-function drawBall(x,y,radius){
+
+function Ball(x,y,radius){
+	this.x=x;
+	this.y=y;
+	this.radius=radius;
+}
+
+
+Ball.prototype.draw=function(){
 	context.beginPath();
-	context.arc(x,y,radius,0,2*Math.PI);
+	context.arc(this.x,this.y,this.radius,0,2*Math.PI);
 	context.strokeStyle="black";
 	context.fillStyle="red";
 	context.stroke();
 	context.fill();
 }
+
+
+Ball.prototype.keyControl=function(){
+	if(LEFT)this.x--;
+	if(RIGHT)this.x++;
+	if(UP)this.y--;
+	if(DOWN)this.y++;
+}
+
 
 window.addEventListener("keydown",function(event){//listen for keys being pressed
 	event.preventDefault();//prevent the browser from scrolling
@@ -31,19 +46,11 @@ window.addEventListener("keyup",function(event){//listen for keys being released
 	if(event.keyCode===40)DOWN=false;
 })
 
-function move(){
-	//change the x and y variables based on the direction variables
-	if(LEFT)x--;
-	if(RIGHT)x++;
-	if(UP)y--;
-	if(DOWN)y++;
-}
-drawBall(x,y,20);
-
+let ball1=new Ball(100,100,25);
 function loop(){
 	context.clearRect(0,0,canvas.clientWidth,canvas.clientHeight);
-	move();
-	drawBall(x,y,20);
+	ball1.keyControl();
+	ball1.draw();
 	window.requestAnimationFrame(loop);
 }
 window.requestAnimationFrame(loop);
